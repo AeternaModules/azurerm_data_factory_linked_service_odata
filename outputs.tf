6bce4ec850c079f1ed8ee33b83280213441be6eb
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:45:14 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_factory_linked_service_odatas" {
+  description = "All data_factory_linked_service_odata resources"
+  value       = azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas
+}
+
