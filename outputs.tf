d5121e5f85229370b9483f286cd2df5537040347
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:56 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,43 +1,38 @@
-output "data_factory_linked_service_odatas" {
-  description = "All data_factory_linked_service_odata resources"
-  value       = azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas
-  sensitive   = true
-}
 output "data_factory_linked_service_odatas_additional_properties" {
-  description = "List of additional_properties values across all data_factory_linked_service_odatas"
-  value       = [for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : v.additional_properties]
+  description = "Map of additional_properties values across all data_factory_linked_service_odatas, keyed the same as var.data_factory_linked_service_odatas"
+  value       = { for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : k => v.additional_properties }
 }
 output "data_factory_linked_service_odatas_annotations" {
-  description = "List of annotations values across all data_factory_linked_service_odatas"
-  value       = [for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : v.annotations]
+  description = "Map of annotations values across all data_factory_linked_service_odatas, keyed the same as var.data_factory_linked_service_odatas"
+  value       = { for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : k => v.annotations }
 }
 output "data_factory_linked_service_odatas_basic_authentication" {
-  description = "List of basic_authentication values across all data_factory_linked_service_odatas"
-  value       = [for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : v.basic_authentication]
+  description = "Map of basic_authentication values across all data_factory_linked_service_odatas, keyed the same as var.data_factory_linked_service_odatas"
+  value       = { for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : k => v.basic_authentication }
   sensitive   = true
 }
 output "data_factory_linked_service_odatas_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_linked_service_odatas"
-  value       = [for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_linked_service_odatas, keyed the same as var.data_factory_linked_service_odatas"
+  value       = { for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : k => v.data_factory_id }
 }
 output "data_factory_linked_service_odatas_description" {
-  description = "List of description values across all data_factory_linked_service_odatas"
-  value       = [for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : v.description]
+  description = "Map of description values across all data_factory_linked_service_odatas, keyed the same as var.data_factory_linked_service_odatas"
+  value       = { for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : k => v.description }
 }
 output "data_factory_linked_service_odatas_integration_runtime_name" {
-  description = "List of integration_runtime_name values across all data_factory_linked_service_odatas"
-  value       = [for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : v.integration_runtime_name]
+  description = "Map of integration_runtime_name values across all data_factory_linked_service_odatas, keyed the same as var.data_factory_linked_service_odatas"
+  value       = { for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : k => v.integration_runtime_name }
 }
 output "data_factory_linked_service_odatas_name" {
-  description = "List of name values across all data_factory_linked_service_odatas"
-  value       = [for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : v.name]
+  description = "Map of name values across all data_factory_linked_service_odatas, keyed the same as var.data_factory_linked_service_odatas"
+  value       = { for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : k => v.name }
 }
 output "data_factory_linked_service_odatas_parameters" {
-  description = "List of parameters values across all data_factory_linked_service_odatas"
-  value       = [for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : v.parameters]
+  description = "Map of parameters values across all data_factory_linked_service_odatas, keyed the same as var.data_factory_linked_service_odatas"
+  value       = { for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : k => v.parameters }
 }
 output "data_factory_linked_service_odatas_url" {
-  description = "List of url values across all data_factory_linked_service_odatas"
-  value       = [for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : v.url]
+  description = "Map of url values across all data_factory_linked_service_odatas, keyed the same as var.data_factory_linked_service_odatas"
+  value       = { for k, v in azurerm_data_factory_linked_service_odata.data_factory_linked_service_odatas : k => v.url }
 }
 
